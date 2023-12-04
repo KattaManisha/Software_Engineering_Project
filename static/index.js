cf0dd37cd4466a95cdec485f678f9ee68bfb14b6
Snehil Shrivastava <Shrivastava.snehil2@gmail.com>
2023-12-04 13:48:29 -0800

    Final implementation of the landing page

--- static/index.js
+++ static/index.js
@@ -176,10 +176,24 @@ function init() {
     loginButton.addEventListener("click", loginUser);
   }
   if (updateButton) {
-    loginButton.addEventListener("click", update_user);
+    updateButton.addEventListener("click", update_user);
   }
 }
 
+function readmore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("readmore-btn");
 
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more..."; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
 
 document.addEventListener("DOMContentLoaded", init);
