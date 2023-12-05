400c2257c11db5c6775bc9bc9b8af87d2c20bf92
KattaManisha <71900049+KattaManisha@users.noreply.github.com>
2023-12-04 17:28:43 -0800

    Final implementation of the landing page (#31)
    * Final implementation of the landing page
    
    * Updating the footer part of the implementation
    
    Signed-off-by: Snehil Shrivastava <Shrivastava.snehil2@gmail.com>
    
    ---------
    
    Signed-off-by: Snehil Shrivastava <Shrivastava.snehil2@gmail.com>
    Co-authored-by: Snehil Shrivastava <Shrivastava.snehil2@gmail.com>

--- static/index.js
+++ static/index.js
@@ -2,120 +2,77 @@ function init() {
   // Function to open the signup modal
   function openSignupModal() {
     var modal = new bootstrap.Modal(
-      document.getElementById('modalRegisterForm')
+      document.getElementById("modalRegisterForm")
     );
     modal.show();
   }
   // Function to open the update modal
   function openUpdateModal() {
-    var modal = new bootstrap.Modal(document.getElementById('modalUpdateForm'));
+    var modal = new bootstrap.Modal(document.getElementById("modalUpdateForm"));
     modal.show();
   }
 
-  // Define the function to navigate to the user"s home page
+  // Define the function to navigate to the user's home page
   function goToUserHomePage() {
-    console.log('Button clicked!'); // For testing purposes
-    // Redirect to the user"s home page
-    window.location.href = '/user-home'; // Replace with the actual URL for the user"s home page
+    console.log("Button clicked!"); // For testing purposes
+    // Redirect to the user's home page
+    window.location.href = "/user-home"; // Replace with the actual URL for the user's home page
   }
 
   // Get the button and attach a click event listener
-  var backButton = document.getElementById('backButton');
+  var backButton = document.getElementById("backButton");
   if (backButton) {
-    backButton.addEventListener('click', goToUserHomePage);
+    backButton.addEventListener("click", goToUserHomePage);
   } else {
-    console.error('Back button element not found.');
+    console.error("Back button element not found.");
   }
 
   // Function to open the login modal
   function openLoginModal() {
-    var modal = new bootstrap.Modal(document.getElementById('modalLoginForm'));
+    var modal = new bootstrap.Modal(document.getElementById("modalLoginForm"));
     modal.show();
   }
 
-  var logoutButton = document.getElementById('logoutBtn');
+  var logoutButton = document.getElementById("logoutBtn");
   if (logoutButton) {
-    logoutButton.addEventListener('click', function () {
+    logoutButton.addEventListener("click", function () {
       // Perform the logout action here, e.g., by redirecting to a logout endpoint
-      window.location.href = '/logout';
+      window.location.href = "/logout";
     });
   }
 
-  function goToFetchHomePage() {
-    console.log('Button clicked!'); // For testing purposes
-    // Redirect to the user"s home page
-    window.location.href = '/fetch-home'; // Replace with the actual URL for the user"s home page
-  }
-
-  var compareButton = document.getElementById('compareButton');
-  if (compareButton) {
-    compareButton.addEventListener('click', goToFetchHomePage);
-  } else {
-    console.error('Fetch Home page not found.');
-  }
-
-  function goToElexPage() {
-    console.log('Button clicked!'); // For testing purposes
-    // Redirect to the user"s home page
-    window.location.href = '/electronics'; // Replace with the actual URL for the user"s home page
-  }
-
-  var electronicsButton = document.getElementById('electronics');
-  if (electronicsButton) {
-    electronicsButton.addEventListener('click', goToElexPage);
-  } else {
-    console.error('Elex page not found.');
-  }
-
-  function goToWatchesPage() {
-    console.log('Button clicked!'); // For testing purposes
-    // Redirect to the user"s home page
-    window.location.href = '/watches'; // Replace with the actual URL for the user"s home page
-  }
-
-  var watchesButton = document.getElementById('watches');
-  if (watchesButton) {
-    watchesButton.addEventListener('click', goToWatchesPage);
-  } else {
-    console.error('Watches page not found.');
-  }
-
-  function goToClothingPage() {
-    console.log('Button clicked!'); // For testing purposes
-    // Redirect to the user"s home page
-    window.location.href = '/clothing'; // Replace with the actual URL for the user"s home page
-  }
-
-  var clothingButton = document.getElementById('clothing');
-  if (clothingButton) {
-    clothingButton.addEventListener('click', goToClothingPage);
-  } else {
-    console.error('Clothing page not found.');
-  }
-
   // Attach click event listeners to the signup and login links
-  var signUpLink = document.getElementById('signUpLink');
-  var loginLink = document.getElementById('loginLink');
-  var updateLink = document.getElementById('updateLink');
+  var signUpLink = document.getElementById("signUpLink");
+  var loginLink = document.getElementById("loginLink");
+  var updateLink = document.getElementById("updateLink");
 
   if (signUpLink) {
-    signUpLink.addEventListener('click', openSignupModal);
+    signUpLink.addEventListener("click", openSignupModal);
   }
 
   if (loginLink) {
-    loginLink.addEventListener('click', openLoginModal);
+    loginLink.addEventListener("click", openLoginModal);
   }
   if (updateLink) {
-    updateLink.addEventListener('click', openUpdateModal);
+    updateLink.addEventListener("click", openUpdateModal);
   }
+
+  const connectUsEmailLink = document.getElementById("connectUsEmailLink");
+
+  connectUsEmailLink.addEventListener("click", (event) => {
+    event.preventDefault(); // Prevent the default link behavior
+
+    // Open the email dialog
+    window.location.href = "mailto:connect@estimateit.com";
+  });
 
   // Function to register a new user
   function registerUser() {
-    var username = document.getElementById('orangeForm-username').value;
-    var first_name = document.getElementById('orangeForm-firstname').value;
-    var last_name = document.getElementById('orangeForm-lastname').value;
-    var email = document.getElementById('orangeForm-email').value;
-    var password = document.getElementById('orangeForm-pass').value;
+    var username = document.getElementById("orangeForm-username").value;
+    var first_name = document.getElementById("orangeForm-firstname").value;
+    var last_name = document.getElementById("orangeForm-lastname").value;
+    var email = document.getElementById("orangeForm-email").value;
+    var password = document.getElementById("orangeForm-pass").value;
 
     var data = {
       username: username,
@@ -125,108 +82,123 @@ function init() {
       password: password,
     };
 
-    fetch('/register', {
-      method: 'POST',
+    fetch("/register", {
+      method: "POST",
       body: JSON.stringify(data),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     })
       .then((response) => response.text())
       .then((data) => {
         console.log(data);
         var registrationResponse = document.getElementById(
-          'registrationResponse'
+          "registrationResponse"
         );
         if (registrationResponse) {
           registrationResponse.innerHTML = data;
         }
 
-        if (data.includes('successful')) {
-          window.location.href = '/user-home'; // Adjust the URL as needed
+        if (data.includes("successful")) {
+          window.location.href = "/user-home"; // Adjust the URL as needed
         }
       });
   }
 
   // Function to update user-info
   function update_user() {
-    var username = document.getElementById('orangeForm-username').value;
-    var first_name = document.getElementById('orangeForm-firstname').value;
-    var last_name = document.getElementById('orangeForm-lastname').value;
+    var username = document.getElementById("orangeForm-username").value;
+    var first_name = document.getElementById("orangeForm-firstname").value;
+    var last_name = document.getElementById("orangeForm-lastname").value;
     var data = {
       username: username,
       first_name: first_name,
       last_name: last_name,
     };
 
-    fetch('/update_user', {
-      method: 'POST',
+    fetch("/update_user", {
+      method: "POST",
       body: JSON.stringify(data),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     })
       .then((response) => response.text())
       .then((data) => {
         console.log(data);
-        var updationResponse = document.getElementById('updationResponse');
+        var updationResponse = document.getElementById("updationResponse");
         if (updationResponse) {
           updationResponse.innerHTML = data;
         }
 
-        if (data.includes('successful')) {
-          window.location.href = '/user_profile'; // Adjust the URL as needed
+        if (data.includes("successful")) {
+          window.location.href = "/user_profile"; // Adjust the URL as needed
         }
       });
   }
 
   // Function to log in a user
   function loginUser() {
-    var email = document.getElementById('orangeForm-email').value;
-    var password = document.getElementById('orangeForm-pass').value;
+    var email = document.getElementById("orangeForm-email").value;
+    var password = document.getElementById("orangeForm-pass").value;
 
     var data = {
       email: email,
       password: password,
     };
 
-    fetch('/login', {
-      method: 'POST',
+    fetch("/login", {
+      method: "POST",
       body: JSON.stringify(data),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     })
       .then((response) => response.text())
       .then((data) => {
         console.log(data);
-        var loginResponse = document.getElementById('loginResponse');
+        var loginResponse = document.getElementById("loginResponse");
         if (loginResponse) {
           loginResponse.innerHTML = data;
         }
 
-        if (data.includes('successful')) {
-          window.location.href = '/user-home'; // Adjust the URL as needed
+        if (data.includes("successful")) {
+          window.location.href = "/user-home"; // Adjust the URL as needed
         }
       });
   }
 
   // Attach click event listeners to the signup and login buttons
-  var signupButton = document.getElementById('signupButton');
-  var loginButton = document.getElementById('loginButton');
-  var updateButton = document.getElementById('updateButton');
-  var compareButton = document.getElementById('compareButton');
+  var signupButton = document.getElementById("signupButton");
+  var loginButton = document.getElementById("loginButton");
+  var updateButton = document.getElementById("updateButton");
 
   if (signupButton) {
-    signupButton.addEventListener('click', registerUser);
+    signupButton.addEventListener("click", registerUser);
   }
 
   if (loginButton) {
-    loginButton.addEventListener('click', loginUser);
+    loginButton.addEventListener("click", loginUser);
   }
   if (updateButton) {
-    loginButton.addEventListener('click', update_user);
+    updateButton.addEventListener("click", update_user);
   }
 }
 
-document.addEventListener('DOMContentLoaded', init);
+function readmore() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("readmore-btn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more...";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+document.addEventListener("DOMContentLoaded", init);
